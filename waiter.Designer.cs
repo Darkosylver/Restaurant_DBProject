13c10327bb2ee2af53b793b0da53fae24bdd9c0f
Amr Ayman <amr.mohamed05@eng-st.cu.edu.eg>
2024-12-10 20:16:10 +0200

    t3bt
    added comments so you can use my dumb functions/queries
    
    almost done with waiter (fadel orders and m3 elsalama)

--- waiter.Designer.cs
+++ waiter.Designer.cs
@@ -44,11 +44,19 @@
             this.delete = new System.Windows.Forms.Button();
             this.items = new System.Windows.Forms.ComboBox();
             this.quantity = new System.Windows.Forms.TextBox();
+            this.freetable = new System.Windows.Forms.Button();
+            this.tablelabel = new System.Windows.Forms.Label();
+            this.restauranttables = new System.Windows.Forms.DataGridView();
+            this.refresh = new System.Windows.Forms.Button();
+            this.addtable = new System.Windows.Forms.Button();
+            this.phonelabel = new System.Windows.Forms.Label();
+            this.deletetable = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.restauranttables)).BeginInit();
             this.SuspendLayout();
             // 
             // order
             // 
-            this.order.Location = new System.Drawing.Point(663, 113);
+            this.order.Location = new System.Drawing.Point(663, 56);
             this.order.Name = "order";
             this.order.Size = new System.Drawing.Size(125, 23);
             this.order.TabIndex = 0;
@@ -57,7 +65,7 @@
             // 
             // update
             // 
-            this.update.Location = new System.Drawing.Point(663, 201);
+            this.update.Location = new System.Drawing.Point(663, 202);
             this.update.Name = "update";
             this.update.Size = new System.Drawing.Size(125, 23);
             this.update.TabIndex = 1;
@@ -67,7 +75,7 @@
             // 
             // reserve
             // 
-            this.reserve.Location = new System.Drawing.Point(663, 361);
+            this.reserve.Location = new System.Drawing.Point(663, 347);
             this.reserve.Name = "reserve";
             this.reserve.Size = new System.Drawing.Size(125, 23);
             this.reserve.TabIndex = 2;
@@ -87,7 +95,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(526, 329);
+            this.label2.Location = new System.Drawing.Point(522, 347);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(85, 16);
             this.label2.TabIndex = 4;
@@ -99,18 +107,20 @@
             this.phoneno.Name = "phoneno";
             this.phoneno.Size = new System.Drawing.Size(126, 22);
             this.phoneno.TabIndex = 5;
+            this.phoneno.TextChanged += new System.EventHandler(this.phoneno_TextChanged);
             // 
             // tableno
             // 
-            this.tableno.Location = new System.Drawing.Point(503, 361);
+            this.tableno.Location = new System.Drawing.Point(503, 377);
             this.tableno.Name = "tableno";
             this.tableno.Size = new System.Drawing.Size(134, 22);
             this.tableno.TabIndex = 6;
+            this.tableno.TextChanged += new System.EventHandler(this.tableno_TextChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(557, 186);
+            this.label5.Location = new System.Drawing.Point(459, 189);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(54, 16);
             this.label5.TabIndex = 9;
@@ -118,15 +128,16 @@
             // 
             // building
             // 
-            this.building.Location = new System.Drawing.Point(541, 213);
+            this.building.Location = new System.Drawing.Point(537, 186);
             this.building.Name = "building";
             this.building.Size = new System.Drawing.Size(100, 22);
             this.building.TabIndex = 12;
+            this.building.TextChanged += new System.EventHandler(this.building_TextChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(425, 186);
+            this.label4.Location = new System.Drawing.Point(473, 220);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(40, 16);
             this.label4.TabIndex = 8;
@@ -135,7 +146,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(300, 186);
+            this.label3.Location = new System.Drawing.Point(486, 248);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(27, 16);
             this.label3.TabIndex = 7;
@@ -144,21 +155,23 @@
             // 
             // street
             // 
-            this.street.Location = new System.Drawing.Point(400, 213);
+            this.street.Location = new System.Drawing.Point(537, 214);
             this.street.Name = "street";
             this.street.Size = new System.Drawing.Size(100, 22);
             this.street.TabIndex = 11;
+            this.street.TextChanged += new System.EventHandler(this.street_TextChanged);
             // 
             // city
             // 
-            this.city.Location = new System.Drawing.Point(256, 214);
+            this.city.Location = new System.Drawing.Point(537, 242);
             this.city.Name = "city";
             this.city.Size = new System.Drawing.Size(100, 22);
             this.city.TabIndex = 10;
+            this.city.TextChanged += new System.EventHandler(this.city_TextChanged);
             // 
             // delete
             // 
-            this.delete.Location = new System.Drawing.Point(663, 230);
+            this.delete.Location = new System.Drawing.Point(663, 231);
             this.delete.Name = "delete";
             this.delete.Size = new System.Drawing.Size(125, 23);
             this.delete.TabIndex = 13;
@@ -169,23 +182,96 @@
             // items
             // 
             this.items.FormattingEnabled = true;
-            this.items.Location = new System.Drawing.Point(256, 113);
+            this.items.Location = new System.Drawing.Point(256, 57);
             this.items.Name = "items";
             this.items.Size = new System.Drawing.Size(121, 24);
             this.items.TabIndex = 14;
             // 
             // quantity
             // 
-            this.quantity.Location = new System.Drawing.Point(400, 114);
+            this.quantity.Location = new System.Drawing.Point(400, 56);
             this.quantity.Name = "quantity";
             this.quantity.Size = new System.Drawing.Size(100, 22);
             this.quantity.TabIndex = 15;
+            // 
+            // freetable
+            // 
+            this.freetable.Location = new System.Drawing.Point(663, 376);
+            this.freetable.Name = "freetable";
+            this.freetable.Size = new System.Drawing.Size(125, 23);
+            this.freetable.TabIndex = 16;
+            this.freetable.Text = "free table";
+            this.freetable.UseVisualStyleBackColor = true;
+            this.freetable.Click += new System.EventHandler(this.freetable_Click);
+            // 
+            // tablelabel
+            // 
+            this.tablelabel.AutoSize = true;
+            this.tablelabel.Location = new System.Drawing.Point(486, 406);
+            this.tablelabel.Name = "tablelabel";
+            this.tablelabel.Size = new System.Drawing.Size(0, 16);
+            this.tablelabel.TabIndex = 17;
+            // 
+            // restauranttables
+            // 
+            this.restauranttables.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.restauranttables.Location = new System.Drawing.Point(12, 257);
+            this.restauranttables.Name = "restauranttables";
+            this.restauranttables.RowHeadersWidth = 51;
+            this.restauranttables.RowTemplate.Height = 24;
+            this.restauranttables.Size = new System.Drawing.Size(403, 181);
+            this.restauranttables.TabIndex = 18;
+            // 
+            // refresh
+            // 
+            this.refresh.Location = new System.Drawing.Point(12, 212);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(75, 23);
+            this.refresh.TabIndex = 19;
+            this.refresh.Text = "refresh";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // addtable
+            // 
+            this.addtable.Location = new System.Drawing.Point(151, 212);
+            this.addtable.Name = "addtable";
+            this.addtable.Size = new System.Drawing.Size(75, 23);
+            this.addtable.TabIndex = 20;
+            this.addtable.Text = "add table";
+            this.addtable.UseVisualStyleBackColor = true;
+            this.addtable.Click += new System.EventHandler(this.addtable_Click);
+            // 
+            // phonelabel
+            // 
+            this.phonelabel.AutoSize = true;
+            this.phonelabel.Location = new System.Drawing.Point(33, 93);
+            this.phonelabel.Name = "phonelabel";
+            this.phonelabel.Size = new System.Drawing.Size(0, 16);
+            this.phonelabel.TabIndex = 21;
+            // 
+            // deletetable
+            // 
+            this.deletetable.Location = new System.Drawing.Point(663, 406);
+            this.deletetable.Name = "deletetable";
+            this.deletetable.Size = new System.Drawing.Size(125, 23);
+            this.deletetable.TabIndex = 22;
+            this.deletetable.Text = "delete table";
+            this.deletetable.UseVisualStyleBackColor = true;
+            this.deletetable.Click += new System.EventHandler(this.deletetable_Click);
             // 
             // waiter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.deletetable);
+            this.Controls.Add(this.phonelabel);
+            this.Controls.Add(this.addtable);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.restauranttables);
+            this.Controls.Add(this.tablelabel);
+            this.Controls.Add(this.freetable);
             this.Controls.Add(this.quantity);
             this.Controls.Add(this.items);
             this.Controls.Add(this.delete);
@@ -205,6 +291,7 @@
             this.Name = "waiter";
             this.Text = "waiter";
             this.Load += new System.EventHandler(this.waiter_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.restauranttables)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -228,5 +315,12 @@
         private System.Windows.Forms.Button delete;
         private System.Windows.Forms.ComboBox items;
         private System.Windows.Forms.TextBox quantity;
+        private System.Windows.Forms.Button freetable;
+        private System.Windows.Forms.Label tablelabel;
+        private System.Windows.Forms.DataGridView restauranttables;
+        private System.Windows.Forms.Button refresh;
+        private System.Windows.Forms.Button addtable;
+        private System.Windows.Forms.Label phonelabel;
+        private System.Windows.Forms.Button deletetable;
     }
 }
